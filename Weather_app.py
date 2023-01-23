f369d22ddcaf636d02fcb9a2f334156666ab745a
stevenbarrow <69319762+stevenbarrow@users.noreply.github.com>
2023-01-23 04:36:21 -0700

    Update Weather_app.py

--- Weather_app.py
+++ Weather_app.py
@@ -19,32 +19,39 @@ This app retrieves the weather from any city in the worls the user choses!
 city = st.text_input('Input a City', 'Park City')
 st.write('Your current selected city is', city)
 
-API_Key = "3014d47cf62ff4109d203a2ec132799a"
+def get_weather(location):
+    
+
+    API_Key = "3014d47cf62ff4109d203a2ec132799a"
 
 #location = input("Enter Your Desired Location: ")
-location = city
+#location = city
 
-geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={location}&appid="
-final_geo_url = geo_url + API_Key
-geo_data = requests.get(final_geo_url).json()
+    geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={location}&appid="
+    final_geo_url = geo_url + API_Key
+    geo_data = requests.get(final_geo_url).json()
 
-keys = [i for i in range(len(geo_data))]
-my_dict = dict(zip(keys, geo_data))
+    keys = [i for i in range(len(geo_data))]
+    my_dict = dict(zip(keys, geo_data))
 
-geo_dict = my_dict[0]
+    geo_dict = my_dict[0]
 
-lat = geo_dict['lat']
-long = geo_dict['lon']
+    lat = geo_dict['lat']
+    long = geo_dict['lon']
 
 #print(lat,long)
 
-part = 'minutely'
+    part = 'minutely'
 
-weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={long}&exclude={part}&appid="
-final_url = weather_url + API_Key
-weather_data = requests.get(final_url).json()
+    weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={long}&exclude={part}&appid="
+    final_url = weather_url + API_Key
+    weather_data = requests.get(final_url).json()
+    return weather_data
 
 #print(weather_data)
 
-st.subheader('The weather in ', city, ' is:')
-st.write(weather_data)
+if st.button('Show me the weather!'):
+    st.subheader('The weather in ', city, ' is:')
+    get_weather(city)
+else:
+    st.write('Click the button to display the weather in your city of choice')
